29734dec02ec03b81ab731d0a13c81016ed9709c
Derek Wilson <derekw@derekw.org>
2022-09-07 12:30:22 +0100

    remove data_load.py

--- data_load.py
+++
@@ -1,8 +0,0 @@
-
-import numpy as np
-import pandas as pd
-from mixupy import mixup
-# from mixupy.mixup import mixup
-
-cam = pd.read_csv('../CambridgeTemperatureNotebooks/data/CamMetCleanish2021.04.26.csv', nrows=1000)
-cam.drop('ds', axis=1, inplace=True)
